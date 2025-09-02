9ec4c14e4884f7aa10eb4d5bfc6400f586764fe9
JaivardhanSinghShaktawat <jaivardhansinghshaktawat5@gmail.com>
2025-09-02 21:05:55 +0530

    Day 6 - Semantic Elements And Structure Of A Web Page

---
+++ 2-HTML/1-HTML-Notes/13-Semantic-Elements.js
@@ -0,0 +1,46 @@
+/* 
+-------------------------------- Semantic Elements --------------------------------
+
+--> What are Semantic Elements ? 
+    Semantic elements are html elements with some meaning.
+    Semantic means meaning.They came in HTML5.
+    Prior to that div element was used for everything.
+    They provide some menaing to the browsers.
+
+    Advantages of semantic elements
+
+    1. It helps browsers and web crawlers to understand the web page.It helps in SEO.
+    2. Helps assistive technologies , screen readers and in accessibility
+    3. It helps developers to understand and structure the content of the web page.
+    It makes the code more readeable and maintainable.
+
+--> Various types of semantic elements
+
+    header : it represents the introductory part of a web page or article 
+    or some section.We can use multiple headers in a single web page.
+
+    nav : it is used for navigation menu.We can also create multiple navigation menu
+    in our web page like in header , sidebar or in footer.But in footer it is not
+    necessary to use the nav element.
+    Try to use aria-label or aria-labelledBy attribute , if you have multiple 
+    navigation menu.
+
+    main : it represents the main content of the web page.
+    There must be one main element per web page.
+    Always put herosection inside the main element.
+
+    section : it defines a section in the document.
+
+    footer : it represents the bottom part of the article or web page.
+
+    time : it represents time. 
+    details and summary : they are used to represent some content with details and
+    summary
+    aside : it represents the sidebar. 
+    article : it is used to represent independent content like blog post etc. 
+    nav : it represents navigation menu. 
+    address : it represents address. 
+
+    There are many semantic elements.
+    Always use semantic elements.
+*/
