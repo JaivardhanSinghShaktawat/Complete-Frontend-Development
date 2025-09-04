140811e04f45ad5493097422d17f38fa98ab0d72
JaivardhanSinghShaktawat <jaivardhansinghshaktawat5@gmail.com>
2025-09-04 19:36:29 +0530

    Day 8 - HTML5 Features

---
+++ 2-HTML/1-HTML-Notes/17-HTML5-Features.js
@@ -0,0 +1,11 @@
+/* 
+----------------------------------- HTML5 Features ---------------------------------
+
+--> Semantic Elements
+    Multimedia elements (audio , video , picture , source)
+    MathML for maths equations.
+    SVG for creating 2D shapes and diagrams. 
+    New input elements came like search , time etc. 
+    doctype declaration changed. 
+    caching of html document and images.
+*/
